1358992bc520ed4982e5d6775180d0623c518a37
Sirzesx <sirzesx@outlook.fr>
2018-11-25 22:15:11 +0100

    Ajout init Sparql Request Console

---
+++ src/Controllers/RequestConstroller.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BTN1.Models;
+using VDS.RDF;
+using VDS.RDF.Writing;
+using VDS.RDF.Parsing;
+using VDS.RDF.Query;
+
+namespace BTN1.Controllers
+{
+    public class RequestController : Controller{
+
+        public IActionResult RequestSparql()
+        {
+
+            //Define a remote endpoint
+            //Use the DBPedia SPARQL endpoint with the default Graph set to DBPedia
+            SparqlRemoteEndpoint endpoint = new SparqlRemoteEndpoint(new Uri("http://dbpedia.org/sparql"), "http://dbpedia.org");
+
+            
+
+            //Make a SELECT query against the Endpoint
+            SparqlResultSet results = endpoint.QueryWithResultSet("SELECT * WHERE {?iri a schema:Movie . ?iri foaf:name ?name .} LIMIT 20");
+            foreach (SparqlResult result in results)
+            {
+                Console.WriteLine(result.ToString());
+            }
+
+
+            Console.WriteLine("------------------------------------------------------------------");
+            //Make a DESCRIBE query against the Endpoint
+            IGraph g = endpoint.QueryWithResultGraph("DESCRIBE");
+            //g.NamespaceMap.AddNamespace("schema", new Uri("http://schema.org/"));
+            foreach (Triple t in g.Triples)
+            {
+                Console.WriteLine(t.ToString());
+            }
+
+            return View();
+        }
+
+
+    }
+
+}
